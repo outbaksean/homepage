61aa0037bfa11068ff0e11f59941736782d0b645
Sean <seanehrig@gmail.com>
2026-02-22 18:18:07 -0500

    Add session data to home page

--- app/src/views/HomeView.vue
+++ app/src/views/HomeView.vue
@@ -1,7 +1,12 @@
 <template>
   <main v-html="content" />
+  <ProjectCard :content="session2" />
+  <ProjectCard :content="session1" />
 </template>
 
 <script setup>
-import content from '../content/home.md'
+  import content from "../content/home.md";
+  import session1 from "../content/sessions/session_2_14_2026.md";
+  import session2 from "../content/sessions/session_2_21_2026.md";
+  import ProjectCard from "../components/ProjectCard.vue";
 </script>
